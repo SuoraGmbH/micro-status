f53ca69b1ed1ff0312dc1918d6818e55fefcde54
Hans-Christian Otto <c.otto@suora.com>
2018-09-21 09:31:43 +0200

    Added TweetList

---
+++ src/component/TweetList.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Tweet } from '../../domain/Tweet';
+import { TweetListItem } from './TweetListItem';
+
+interface Props {
+  tweets: Tweet[];
+}
+
+export class TweetList extends React.Component<Props> {
+  public render() {
+    const { tweets } = this.props;
+    return (
+      <div>
+        {tweets.map(tweet => (
+          <TweetListItem tweet={tweet} />
+        ))}
+      </div>
+    );
+  }
+}
---
+++ src/component/TweetListItem.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Tweet } from '../../domain/Tweet';
+
+interface Props {
+  tweet: Tweet;
+}
+
+export class TweetListItem extends React.Component<Props> {
+  public render() {
+    const { tweet } = this.props;
+    return (
+      <div>
+        <h6>{tweet.userId}</h6>
+        <p>{tweet.message}</p>
+      </div>
+    );
+  }
+}
