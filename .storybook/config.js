8cbed2bccff1386049e9dace16968e3fc4c63276
Hans-Christian Otto <c.otto@suora.com>
2018-09-24 09:48:44 +0200

    Added storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.(jsx?|tsx)$/);
+// const req = require.context('../src', true, /.stories.(jsx?|tsx)$/);
 function loadStories() {
-    req.keys().forEach((filename) => req(filename));
+    require('../src/stories.index');
+    // req.keys().forEach((filename) => req(filename));
 }
 
 configure(loadStories, module);
