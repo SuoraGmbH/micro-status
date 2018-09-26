f03eb59487be9c952a119b9f899c6c0ecc73cc8d
Hans-Christian Otto <c.otto@suora.com>
2018-09-26 17:24:21 +0200

    fixes for ui update

--- src/component/TweetListItem.stories.tsx
+++ src/component/TweetListItem.stories.tsx
@@ -3,12 +3,14 @@ import * as React from 'react';
 import { TweetListItem } from './TweetListItem';
 
 storiesOf('Welcome', module).add('foo', () => (
-  <TweetListItem
-    tweet={{
-      message: 'Hello World!',
-      userId: 'tobmaster',
-      id: 3,
-      date: '2018-09-24T09:16:52+0200',
-    }}
-  />
+  <div className="timeline">
+    <TweetListItem
+      tweet={{
+        message: 'Hello World!',
+        userId: 'tobmaster',
+        id: 3,
+        date: '2018-09-24T09:16:52+0200',
+      }}
+    />
+  </div>
 ));
